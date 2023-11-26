1fdcdbb6d3f5f96d37a5d7eaad6d866c9932fd1a
Fernando dos Santos Vieira <nandovieira2@yahoo.com.br>
2023-11-26 14:24:44 -0300

    - Creation of the Spring Boot Project - Update of the .gitignore file

---
+++ src/main/java/br/com/financial/remittance/FinancialRemittanceServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.financial.remittance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialRemittanceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialRemittanceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/financial/remittance/FinancialRemittanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.financial.remittance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialRemittanceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
